2e65bb3b725f2e2b9be064a1c661a866b61edfb5
kepppuw_70 <tsgdnepro@3g.ua>
2020-09-03 14:45:43 +0300

    Add HW2

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Домашнее задание №2 - главная</title>
+   <link rel="stylesheet" type="text/css" href="css/style1.css">
+  </head>
+  <body>
+  
+
+<nav class="menu">
+  <ul>
+    <li><a href="#">Главная</a></li>
+    <li><a href="table_table.php">Таблица-table</a></li>
+    <li><a href="table_div.php">Таблица-div</a></li>
+  </ul>
+</nav>
+<br><br><br>
+    <h1>Домашнее задание-2</h1>
+
+  </body>
+</html>
